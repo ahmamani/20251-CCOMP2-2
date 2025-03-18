c3066fc0574745974c22ccbfd8ba7df66b7f47b6
DOCENTEP-104 <DOCENTEP-104@UCSP.AULAB>
2025-03-18 08:26:06 -0500

    Tipo de dato int

---
+++ SumaDeDosNumeros/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* Tipos de datos basicos */
+    /**
+        Para trabajar con numeros enteros se utilizan los tipos
+        - short
+        - int
+        - long
+        - long long
+    */
+    /**
+        Para declarar una variable en c++
+            TIPO NOMBRE_VARIABLE;
+    */
+    int val; // declarando una variable de tipo int(entero) que se llama val
+    val = 10;
+    cout << "El valor de la variable es: "  << val << endl;
+    return 0;
+}
