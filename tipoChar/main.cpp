5106a974ba2a88c8f4fe6ebcf2c466b4c39c79af
DOCENTE-N412 <DOCENTE-N412@UCSP.AULAB>
2025-03-19 08:24:52 -0500

    ejemplo de tipo char

---
+++ tipoChar/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /**
+        el caracter se representa con las comillas simples ''
+        la cadena (string) se representa con las comillas dobles ""
+    */
+    char val = 'a';
+    int valNum = static_cast<int>(val);
+    cout << val << " su valor en numero es: " << valNum << endl;
+    return 0;
+}
