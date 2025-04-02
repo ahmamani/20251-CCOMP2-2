f16ae62e7b927530bba5a29c9aaa94c5a2476864
DOCENTE-N412 <DOCENTE-N412@UCSP.AULAB>
2025-04-02 08:26:05 -0500

    while ejercicios

--- 3_While_ej_2_palindrome/main.cpp
+++ 3_While_ej_2_palindrome/main.cpp
@@ -9,6 +9,30 @@ using namespace std;
 **/
 int main()
 {
+    bool f=true;
+    string palabra;
+    char c;
+    cout<<"ingrese una palabra:  ";
+    getline(cin,palabra);
+    int len=(palabra.length()/2), flag=0,flg1=0, flg2=palabra.length()-1;
+    while (f){
+        if(palabra.at(flag)==palabra.at(flg2)){
+                flag++;
+                flg2--;
+                if(flag>=len && flg2<len){
+                    cout<<"la palabra es un palindromo"<<endl;
+                    f=false;
+                }
+        }
+        else{
+            cout<<"la palabra no es un palindromo";
+            f=false;
+        }
+    }
+
+
+
+    /*
     string cadena;
     cout << "Ingrese una cadena por favor: ";
     getline(cin, cadena);
@@ -18,7 +42,7 @@ int main()
 
     while( ini < fin ) {
 
-    }
+    }*/
 
     return 0;
 }
--- 3_While_ej_4_substring/main.cpp
+++ 3_While_ej_4_substring/main.cpp
@@ -7,12 +7,42 @@ using namespace std;
 * incluyendo espacios en blanco y una palabra e indique si
 * dicha palabra se encuentra en dicha cadena y cuantas veces.
 * Ejemplo:
-*       "ciencia de la computacion de la ucsp"  -> cadena
+*       "ciendecia de la computacion de la ucsp"  -> cadena
 *       "de"                                    -> palabra
-*       La palabra 'de' se encuentra 2 veces.
+*       La palabra 'de' se encuentra 3 veces.
 **/
 int main()
 {
-    cout << "Hello world!" << endl;
+    string cad;
+    string palabra;
+    cout << "Ingrese una frase: ";
+    getline(cin, cad);
+    cout << "Ingrese una palabra a buscar: ";
+    cin >> palabra;
+
+    int lencad = cad.length();
+    int idxcad = 0;
+    int lenpal = palabra.length();
+    int idxpal = 0;
+    int counter = 0;
+    bool flag = true;
+    while(idxcad < lencad) {
+        if(cad.at(idxcad) == palabra.at(idxpal)) {
+            while(idxpal < lenpal) {
+                if(cad.at(idxcad) != palabra.at(idxpal)) {
+                    flag = false;
+                    break;
+                }
+                idxpal++;
+                idxcad++;
+            }
+            if(flag) {
+                counter++;
+            }
+            idxpal = 0;
+        }
+        idxcad = idxcad + 1;
+    }
+    cout << counter << endl;
     return 0;
 }
